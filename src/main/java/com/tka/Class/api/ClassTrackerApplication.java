cf6113b6fb117d3d38ef9daa3a106129d320d985
dalavitrupti2001 <dalavitrupti2001@gmail.com>
2024-10-10 12:02:31 +0530

    first commit

---
+++ src/main/java/com/tka/Class/api/ClassTrackerApplication.java
@@ -0,0 +1,15 @@
+ package com.tka.Class.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassTrackerApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassTrackerApplication.class, args);
+	}
+
+}
+ 
---
+++ src/main/java/com/tka/Class/api/exceptions/DivideByZeroException.java
@@ -0,0 +1,8 @@
+package com.tka.Class.api.exceptions;
+
+public class DivideByZeroException extends RuntimeException{
+
+	public DivideByZeroException(String msg) {
+		super(msg);
+	}
+}
